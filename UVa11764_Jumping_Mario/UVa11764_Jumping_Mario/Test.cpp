0d5ddc2e8f42e23b5521d78b931ff53cba2ae53c
zoooooa123 <zoooooa123@gmail.com>
2017-05-17 00:01:04 +0800

    Add a new failed test

--- UVa11764_Jumping_Mario/UVa11764_Jumping_Mario/Test.cpp
+++ UVa11764_Jumping_Mario/UVa11764_Jumping_Mario/Test.cpp
@@ -1,1 +1,11 @@
 #include "gtest/gtest.h"
+#include "Mario.h"
+#include <vector>
+#include <utility>
+using namespace std;
+
+TEST(Mario, CalculateJumpUpAndDown)
+{
+    vector<unsigned int> jump = { 1,2,3,4,5 };
+    EXPECT_EQ(make_pair(4, 0), Mario::CalculateJump(jump.begin()));
+}
