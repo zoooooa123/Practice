0c2c4b39317aeaee9c14275b129f710dc4f2246b
zoooooa123 <zoooooa123@gmail.com>
2017-05-16 23:16:28 +0800

    Add failed test for "CalculateJump"

---
+++ UVa11764_Jumping_Mario/UVa11764_Jumping_Mario/Mario.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include <utility>
+
+class Mario 
+{
+public:
+    static std::pair<unsigned int, unsigned int> CalculateJump(std::vector<unsigned int>::const_iterator iter)
+    {
+        return std::make_pair(0,0);
+    }
+};
