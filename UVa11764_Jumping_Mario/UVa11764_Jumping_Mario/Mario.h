7771d0e7cf8b60f2270f17d0530777b564baab3f
zoooooa123 <zoooooa123@gmail.com>
2017-05-17 00:00:38 +0800

    Overload  operator== to compare two pair

--- UVa11764_Jumping_Mario/UVa11764_Jumping_Mario/Mario.h
+++ UVa11764_Jumping_Mario/UVa11764_Jumping_Mario/Mario.h
@@ -5,8 +5,15 @@
 class Mario 
 {
 public:
-    static std::pair<unsigned int, unsigned int> CalculateJump(std::vector<unsigned int>::const_iterator iter)
+    static std::pair<int, int> CalculateJump(std::vector<unsigned int>::const_iterator iter)
     {
         return std::make_pair(0,0);
     }
 };
+
+bool operator==(const std::pair<int, int> &lhs, const std::pair<int, int> &rhs)
+{
+    return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
+
