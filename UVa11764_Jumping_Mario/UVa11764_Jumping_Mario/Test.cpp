e9f79e1f9b145eeef12b983eef48cbff8b98e984
zoooooa123 <zoooooa123@gmail.com>
2017-05-17 00:12:56 +0800

    Add more test case

--- UVa11764_Jumping_Mario/UVa11764_Jumping_Mario/Test.cpp
+++ UVa11764_Jumping_Mario/UVa11764_Jumping_Mario/Test.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 TEST(Mario, CalculateJumpUpAndDown)
 {
-    vector<unsigned int> jump = { 1,2,3,4,5 };
-    EXPECT_EQ(make_pair(4, 0), Mario::CalculateJump(jump));
+    vector<unsigned int> jump1 = { 1,4,2,2,3,5,3,4 };
+    EXPECT_EQ(make_pair(4, 2), Mario::CalculateJump(jump1));
+
+    vector<unsigned int> jump2 = { 9 };
+    EXPECT_EQ(make_pair(0, 0), Mario::CalculateJump(jump2));
+
+    vector<unsigned int> jump3 = { 1,2,3,4,5 };
+    EXPECT_EQ(make_pair(4, 0), Mario::CalculateJump(jump3));
 }
