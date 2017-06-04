9b1585313a84d74bcba638951a06a45063d04ac5
zoooooa123 <zoooooa123@gmail.com>
2017-06-05 01:25:51 +0800

    Implement  "test_case" function deal with each request

--- UVa10242/UVa10242/main.cpp
+++ UVa10242/UVa10242/main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
-string Calulate()
+struct Point
 {
-    return string();
+    double x = 0.0;
+    double y = 0.0;
+};
+
+ostream& operator >> (ostream& outstream, const Point& P)
+{
+    cout << P.x << " " << P.y << endl;
+    return outstream;
+}
+
+void test_case(const string& input)
+{
+    Point p1,p2,p3,p4;
+    stringstream ss(input);
+    ss >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y >> p4.x >> p4.y;
+    vector<Point> allPoint = { p1, p2, p3, p4};
 };
 
 int main()
 {
-    string input;
-    while (getline(cin, input))
+    string eachLine;
+    while (getline(cin, eachLine))
     {
-        Calulate();
+        test_case(eachLine);
     }
     return 0;
 }
