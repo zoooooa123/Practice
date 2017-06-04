c70c6764973882d20acda04b38bf739bed9dbeb0
zoooooa123 <zoooooa123@gmail.com>
2017-06-04 21:50:25 +0800

    Initialize directory

---
+++ UVa10242/UVa10242/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Calulate()
+{
+    return string();
+};
+
+int main()
+{
+    string input;
+    while (getline(cin, input))
+    {
+        Calulate();
+    }
+    return 0;
+}
